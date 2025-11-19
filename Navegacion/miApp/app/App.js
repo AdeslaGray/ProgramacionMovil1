e48e0556df93e518b5b1f146d3f234b0a67ba60a
Adela I. Garay <adelaisabelgarayalvarado@gmail.com>
2025-11-18 22:51:42 -0600

    agregando tarea de navegacion movil
    Tarea de navegacion

---
+++ Navegacion/miApp/app/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>¡Hola, Isa desde Expo!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  texto: {
+    fontSize: 20,
+    color: 'purple',
+  },
+});
